463240fbb4fbac3775c787511d96b1a06443817d
Shial <shial@bitbucket.org>
2017-12-12 18:55:28 +1100

    [0.1.0] Create SLazeKit class

--- Sources/SLazeKit/StringInitializable.swift
+++ Sources/SLazeKit/StringInitializable.swift
@@ -9,7 +9,6 @@ extension Int: StringInitializable {
         guard let value = Int(rawValue) else {
             return nil
         }
-        
         self = value
     }
 }
@@ -19,7 +18,6 @@ extension Double: StringInitializable {
         guard let value = Double(rawValue) else {
             return nil
         }
-        
         self = value
     }
 }
@@ -29,21 +27,28 @@ extension KeyedDecodingContainerProtocol {
         guard contains(key) else {
             return nil
         }
-        
         guard !(try decodeNil(forKey: key)) else {
             return nil
         }
-        
         if let string = try? decode(String.self, forKey: key) {
             guard let value = T.init(rawValue: string) else {
                 throw DecodingError.dataCorruptedError(forKey: key,
                                                        in: self,
                                                        debugDescription: "Found string that cannot be converted to \(T.self)")
             }
-            
             return value
         }
-        
+        return try decode(T.self, forKey: key)
+    }
+    func decodeUnstable<T: Decodable & StringInitializable>(_ type: T.Type = T.self, forKey key: Key) throws -> T {
+        if let string = try? decode(String.self, forKey: key) {
+            guard let value = T.init(rawValue: string) else {
+                throw DecodingError.dataCorruptedError(forKey: key,
+                                                       in: self,
+                                                       debugDescription: "Found string that cannot be converted to \(T.self)")
+            }
+            return value
+        }
         return try decode(T.self, forKey: key)
     }
 }
